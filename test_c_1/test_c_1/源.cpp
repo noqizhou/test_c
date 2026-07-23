d4a601f13657541a5c60ee73bb8df2c626ac1fae
noqi <2460846216@qq.com>
2026-07-24 07:45:58 +0800

    第一次编程作业

---
+++ test_c_1/test_c_1/源.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("I lost my phone!");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     man\n");
+//	return 0;
+//}
+
+int main()
+{
+	printf("     **     \n");
+	printf("     **     \n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *    \n");
+	printf("    *  *    \n");
+	return 0;
+    
+}
