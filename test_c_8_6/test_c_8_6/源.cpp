02154d2f760c7d88db7946a6f5c3237edf58167a
noqi <2460846216@qq.com>
2026-08-06 10:26:09 +0800

    8.6课上代码练习：字符，全局变量和局部变量，\ddd,\xhh

---
+++ test_c_8_6/test_c_8_6/源.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+//int main()
+//{
+//	short s = 12;
+//	int b = 10;
+//	printf("%zu\n", sizeof(s = b + 1));//sizeof输出的是字节长度
+//	printf("%d\n", s);
+//
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	printf("%d\n", 71);
+//	printf("%d\n", 071);//0代表八进制；0x代表十六进制；
+//	
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	printf("%c\n", 'X');
+//	printf("%c\n", 'x');
+//	printf("%c\n", '\130');//\ddd 1-3位八进制数字
+//	printf("%c\n", '\x51');//\xhh 1-2位十六进制数字
+//
+//	return 0;
+//}
+
+
+//int num1 = 30;//全局变量，全都可以用
+//int main()
+//{
+//	printf("%d\n", num1);
+//	int num2 = 10;//局部变量，局部里可用
+//	{
+//		int num3 = 110;//局部变量，局部里可用
+//		printf("%d\n", num1);
+//	}
+//	printf("%d\n", num1);
+//
+//	return 0;
+//}
+
+
+int num1 = 20;
+int main()
+{
+	int num1 = 10;
+	printf("%d\n", num1);
+
+	return 0;
+}
+
