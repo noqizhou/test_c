664ac8d47d10a836020c0bc22109f7d72868030c
noqi <2460846216@qq.com>
2026-08-07 19:57:32 +0800

    课堂练习：运算符，求模，赋值运算（从右到左赋值）

---
+++ test_c_8_7/test_c_8_7/源.cpp
@@ -0,0 +1,69 @@
+#define CRT SECURE NO WARNINGS
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int a = 5;
+//	int b = 7;
+//
+//	printf("%d\n", 5 + 7);
+//	printf("%d\n", a + b);
+//	printf("%d\n", 5 - 7);
+//	printf("%d\n", 5 * 7);
+//	printf("%d\n", 5 / 7);
+//	printf("%f\n", 5.0 / 7);
+//	printf("%f\n", a / b);
+//	printf("%d\n", a ^ b);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int score = 5;
+//	//score = (score / 20) * 100;
+//	//score = (score / 20.0) * 100;
+//	score = (score % 20) * 100;
+//	printf("%d\n", score);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", 6 % 4);//取模：取余数
+//	printf("%d\n", 1025 % 10);//计算一个整数的最后一位
+//
+//	return 0;
+//}
+
+
+
+
+//赋值运算
+//int main()
+//{
+//	//int a = 10;//初始值
+//	//a = 20;//赋值
+//	//a = 3 + 5;//再赋值
+//	//printf("%d\n", a);//结果为8
+//
+//
+//
+//	int a = 4;//初始值
+//	int b = 5;//初始值
+//	int c = 7;//初始值
+//	//c = b = a - 3;//从右到左计算，依次赋值，答案为1。注意：b 不可以再计算
+//	b = a - 3;
+//	c = b - 2;//拆开后，b 可以再计算
+//    
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+
+//复合赋值
