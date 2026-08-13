bca72677ef29e8f6f7dc4da76a4721b96506ee12
noqi <2460846216@qq.com>
2026-08-13 20:12:15 +0800

    数据类型和变量作业

---
+++ test_c_8_13/test_c_8_13/源.cpp
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//全局变量和局部变量名称相同，局部变量优先使用
+
+
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b) != EOF) {
+//		//scanf如果输出EOF，说明循环结束
+//		printf("%d %d", a / b , a % b);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a;
+//	while (scanf("%d", &a) != EOF) {
+//		if (a >= 140) {
+//			printf("Genius\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d %d", &a, &b) != EOF) {
+//		if (a > b) 
+//		{
+//			printf("%d>%d\n", a, b);
+//		}
+//		else if (a == b)
+//		{
+//			printf("%d=%d\n", a, b);
+//		}
+//		else (a < b)
+//		{
+//			printf("%d<%d\n", a, b);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a;
+//	while (scanf("%d", &a) != EOF) {
+//		if (a % 5 == 0)
+//		{
+//			printf("YES\n");
+//		}
+//		else if (a % 5 != 0)
+//		{
+//			printf("NO\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+	int a;
+	while (scanf("%d", &a) != EOF) {
+		int i = 0;
+		while (i < a)
+		{
+			i++;
+			printf("*");
+
+		}
+		printf("\n");
+	}
+	return 0;
+}
